e9e337d0c51c3c29349829a856528d5f4770daf8
MinxiangPan <pmx971114@gmail.com>
2020-03-12 18:29:08 -0400

    half way down for folder page, need fix on CORS policy for axios

--- src/components/folder/folder.js
+++ src/components/folder/folder.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Folder extends Component {
     constructor(props){
         super(props);
-
+        this.state = {
+            list : [],
+            server : 'https://www.mattpan.com:8443'
+        };
     }
+
+    componentDidMount(){
+        var header = {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin' : '*',
+            'Connection' : 'keep-alive'
+        };
+        axios.get(this.state.server + '/public', {headers : header})
+        .then(res => {
+            let nlist = []
+            if(!res.message.hasOwnProperty("errno")){
+                nlist = res.message.split('\n');   
+            }
+            this.setState({list: res.message});
+            console.log(this.state.list);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
+
 
     render() {
         return (
-        <div> textInComponent </div>
+        <div>
+
+        </div>
         );
     }
 }
