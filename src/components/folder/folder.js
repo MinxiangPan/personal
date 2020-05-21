f504203192d7733e54b463a2ac121131293e1d8f
MinxiangPan <pmx971114@gmail.com>
2020-05-21 15:29:20 -0400

    fix github page issue

--- src/components/folder/folder.js
+++ src/components/folder/folder.js
@@ -10,25 +10,25 @@ class Folder extends Component {
         };
     }
 
-    componentDidMount(){
-        var header = {
-            'Access-Control-Allow-Origin' : 'localhost:3000',
-            'Connection' : 'keep-alive'
-        };
+    // componentDidMount(){
+    //     var header = {
+    //         'Access-Control-Allow-Origin' : 'localhost:3000',
+    //         'Connection' : 'keep-alive'
+    //     };
         
-        axios.get(this.state.server + '/public', {headers : header})
-        .then(res => {
-            let nlist = []
-            if(!res.message.hasOwnProperty("errno")){
-                nlist = res.message.split('\n');   
-            }
-            this.setState({list: res.message});
-            console.log(this.state.list);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
+    //     axios.get(this.state.server + '/public', {headers : header})
+    //     .then(res => {
+    //         let nlist = []
+    //         if(!res.message.hasOwnProperty("errno")){
+    //             nlist = res.message.split('\n');   
+    //         }
+    //         this.setState({list: res.message});
+    //         console.log(this.state.list);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     });
+    // }
 
 
 
